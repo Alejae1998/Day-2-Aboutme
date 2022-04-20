a344c17be3eb85f6c07baacc791aba323418328c
Alejae1998 <alejagr1998@gmail.com>
2022-04-20 16:28:20 -0700

    button

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 // import functions
 
 // grab DOM elements
-
-// set event listeners 
-    // get user input
-    // use user input to update state 
-    // update DOM to reflect the new state
+const button = document.getElementById('show-more');
+const list = document.getElementById('more');
+button.onclick = function() {
+    if (list.style.display !== 'none') {
+        list.style.display = 'none';
+    } else {
+        list.style.display = 'block';
+    }
+};
+// set event listeners
+// get user input
+// use user input to update state
+// update DOM to reflect the new state
